de18d0c61876ef3c2d99bf10eb28aaf40da469d6
金小虎 <XiaoHu.Jin@merchant-s.com>
2016-05-21 11:38:11 +0800

    5-20

--- src/application/views/common/sidebar_start.php
+++ src/application/views/common/sidebar_start.php
@@ -17,8 +17,8 @@
 .sidebar .level2{
   padding: 7px 30px;
 }
-.sidebar .level2:last-child{
-  margin-bottom: 20px;
+.sidebar .li_sep{
+  height: 10px;
 }
 
 .sidebar li:hover{
@@ -56,15 +56,21 @@ h1{
       <li id="store_flow" class="level2"><a href="<?php echo $base_path;?>service/store_flow">導入までの流れ</a></li>
       <li id="store_faq" class="level2"><a href="<?php echo $base_path;?>service/store_faq">FAQ</a></li>          
       
+      <div class="li_sep"></div>
+
       <li id="web" class="web level1"><a href="<?php echo $base_path;?>service/web">クレジットカード決済（早期）＜WEB＞</a></li>
       <li id="web_merit" class="web level2"><a href="<?php echo $base_path;?>service/web_merit">特徴・メリット</a></li>
       <li id="web_service" class="web level2"><a href="<?php echo $base_path;?>service/web_service">サービスの仕組み</a></li>
       <li id="web_flow" class="web level2"><a href="<?php echo $base_path;?>service/web_flow">導入までの流れ</a></li>
       <li id="web_price" class="web level2"><a href="<?php echo $base_path;?>service/web_price">料金表</a></li>
-      <li id="web_faq" class="web level2"><a href="<?php echo $base_path;?>service/web_faq">FAQ</a></li>          
+      <li id="web_faq" class="web level2"><a href="<?php echo $base_path;?>service/web_faq">FAQ</a></li>    
+
+      <div class="li_sep"></div> 
 
       <li id="wechat" class="level1"><a href="<?php echo $base_path;?>service/wechat">WeChat決済</a></li>
       <li id="wechat_merit" class="level2"><a href="<?php echo $base_path;?>service/wechat_merit">特徴・メリット</a></li> 
+
+      <div class="li_sep"></div>
 
       <li id="wechatad" class="level1"><a href="<?php echo $base_path;?>service/wechatad">WeChat広告<br/>プロモーション (公式アカウント作成)</a></li>
       <li id="wechatad_merit" class="level2"><a href="<?php echo $base_path;?>service/wechatad_merit">特徴・メリット</a></li>
